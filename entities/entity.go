431c6e95cfff261837be6df493f1244a3766af7e
bpatyi <b.patyi@mito.hu>
2019-09-03 11:21:58 +0200

    fix: complete entity struct

--- entities/entity.go
+++ entities/entity.go
@@ -6,4 +6,5 @@ type Entity struct {
 	Entities   *Entities `json:"entities,omitempty"`
 	Value      string    `json:"value"`
 	Type       string    `json:"type"`
+	Suggested  bool      `json:"suggested"`
 }
